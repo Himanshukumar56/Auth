77676dde6d42c9114265963b44ad91055d0e09eb
Himanshu Kumar <himanshu.2201056cs@iiitbh.ac.in>
2024-12-11 23:31:27 +0530

    Commit-2

---
+++ backend/utils/generateTokenAndSetCookie.js
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken";
+
+export const generateTokenAndSetCookie = (res,userId) => {
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+        expiresIn : "7d",
+    });
+    res.token("token",token,{
+        
+    })
+}
