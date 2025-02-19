7fb2805e5a77dc09f4272b2ab8d5038a01937cb1
Himanshu Kumar <himanshu.2201056cs@iiitbh.ac.in>
2025-02-20 03:51:11 +0530

    Backend work completed, including all the routes and verification token routes as well

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./DB/connectDB.js";
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); //allows us to parse incoming requests: req.body
+app.use(cookieParser()); // allows to parse incoming cookies
 
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   connectDB();
   console.log("Server is running on port: ", PORT);
 });
-
