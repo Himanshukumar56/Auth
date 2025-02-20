2e1c88dc3fc7130f31d03e40263e5326009b45bb
Himanshu Kumar <himanshu.2201056cs@iiitbh.ac.in>
2025-02-21 03:47:14 +0530

    Completed the project, built the frontend and then connected it with the backend workflow

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+
 import cookieParser from "cookie-parser";
 import { connectDB } from "./DB/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json()); //allows us to parse incoming requests: req.body
 app.use(cookieParser()); // allows to parse incoming cookies
 
